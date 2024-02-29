123f99f64762fa2434bcee4682e9b5a0718aa98e
XaviVillamizar <88797841+XaviVillamizar@users.noreply.github.com>
2024-02-29 17:03:14 -0500

    Add files via upload

---
+++ Ejercicios2/Array/Ejercicio3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GIU;
+import java.util.Scanner;
+
+//Escribe un programa que pida 20 números enteros. Estos números se deben
+//introducir en un array de 4 filas por 5 columnas. El programa mostrará las sumas
+//parciales de filas y columnas igual que si de una hoja de cálculo se tratara. La
+//suma total debe aparecer en la esquina inferior derecha.
+
+public class Array_N3 {
+    public static void main (String[]args){
+        Scanner teclado = new Scanner (System.in);
+        
+        
+    }
+    
+}
