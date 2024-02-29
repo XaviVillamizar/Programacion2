8a7e810ec0081263b1c6c91a60f76e4bb94eec0c
XaviVillamizar <88797841+XaviVillamizar@users.noreply.github.com>
2024-02-29 16:35:21 -0500

    Xavi ejercicios 1

---
+++ Ejercicios1/Basicos/Ejercicio15.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller;
+import java.util.Scanner;
+/**
+ *
+ * @author LabSispc01
+ */
+public class dosVariables {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Valor de A: ");
+        int valorA = scanner.nextInt();
+        
+    System.out.print("Valor de B: ");
+        int valorB = scanner.nextInt();
+        
+    System.out.print("Los valores intercambiados de A y B son, A: "+valorB+" B: "+valorA );
+        
+    
+    
+}
+}
